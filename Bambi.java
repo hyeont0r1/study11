78d0841bfa2701a2f26c9b3fc3b1f69f528c9f7f
Lee Hyeon Jeong <hyeont0r1@gmail.com>
2018-05-25 02:50:11 +0900

    My third commit

--- Bambi.java
+++ Bambi.java
@@ -2,5 +2,6 @@ public class Bambi {
     public static void main(String[] args){
         System.out.println("Hello world");
         System.out.println("yeeeeeeeeeee");
+        System.out.println("aaaaaaaa");
     }
 }
